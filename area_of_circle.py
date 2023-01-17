32805f13cf459dc66bee00903613591a8f04d925
Tashif Jawad <121769288+tashif-rbg@users.noreply.github.com>
2023-01-17 18:18:51 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,8 @@
+# Python program which accepts the radius of a circle from the user and computes area.
+r = input("enter the radius of circle :")
+radius = float(r)
+print(radius)
+
+area = 3.14*radius*radius
+
+print('area of a circle with radius 1.1 is : ',area)
